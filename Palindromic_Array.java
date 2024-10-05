62bc4f0abbae2dc244addb00aebba8ee246aed19
Satish kumar <156699210+satishmuz73@users.noreply.github.com>
2024-10-05 23:16:08 +0530

    Update Palindromic_Array.java

--- Palindromic_Array.java
+++ Palindromic_Array.java
@@ -1,3 +1,20 @@
+// Examples:
+
+// Input: arr[] = [111, 222, 333, 444, 555]
+// Output: true
+// Explanation:
+// arr[0] = 111, which is a palindrome number.
+// arr[1] = 222, which is a palindrome number.
+// arr[2] = 333, which is a palindrome number.
+// arr[3] = 444, which is a palindrome number.
+// arr[4] = 555, which is a palindrome number.
+// As all numbers are palindrome so This will return true.
+
+// Input: arr[] = [121, 131, 20]
+// Output: false
+// Explanation: 20 is not a palindrome hence the output is false.
+
+
 class Solution {
     public static boolean palinArray(int[] arr) {
         // add code here.
