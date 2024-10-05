646d9774ceff7b1a9d7dd3d7330e3b0cf0113066
Satish kumar <156699210+satishmuz73@users.noreply.github.com>
2024-10-05 23:00:16 +0530

    Create Palindromic_Array.java

---
+++ Palindromic_Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static boolean palinArray(int[] arr) {
+        // add code here.
+        for(int i=0; i<arr.length; i++){
+            int temp = arr[i];
+            int rev = 0;
+            
+            while(temp != 0){
+                int rem = temp % 10;
+                rev = rev * 10 + rem;
+                temp = temp /10;
+            }
+            
+            if(arr[i] != rev){
+                return false;
+            }
+        }
+        return true;
+    }
+}
